b578c793a9aac22a99ee532be35b6a7e0aa9e1e0
Holger Rapp <hrapp@google.com>
2017-07-18 10:47:36 +0200

    Figured out IMU data is wonky. Stopped investigation here.

--- cartographer_ros/configuration_files/eric_config.lua
+++ cartographer_ros/configuration_files/eric_config.lua
@@ -20,7 +20,7 @@ options = {
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
   tracking_frame = "base_link",
-  published_frame = "base_footprint",
+  published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = false,
   use_odometry = false,
@@ -33,17 +33,24 @@ options = {
   trajectory_publish_period_sec = 30e-3,
 }
 
+NUM_RANGE_DATA = 120
+
 TRAJECTORY_BUILDER_3D.scans_per_accumulation = 1
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = NUM_RANGE_DATA
+TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.min_num_points = 800.
 
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
-MAP_BUILDER.sparse_pose_graph.optimization_problem.huber_scale = 5e2
-MAP_BUILDER.sparse_pose_graph.optimize_every_n_scans = 320
-MAP_BUILDER.sparse_pose_graph.constraint_builder.sampling_ratio = 0.03
-MAP_BUILDER.sparse_pose_graph.optimization_problem.ceres_solver_options.max_num_iterations = 10
+-- SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 5e2
+SPARSE_POSE_GRAPH.optimize_every_n_scans = 0
+SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 0.3
+SPARSE_POSE_GRAPH.constraint_builder.max_constraint_distance = 50.
+SPARSE_POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 30.
+
+-- SPARSE_POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
 -- Reuse the coarser 3D voxel filter to speed up the computation of loop closure
 -- constraints.
 MAP_BUILDER.sparse_pose_graph.constraint_builder.adaptive_voxel_filter = TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter
-MAP_BUILDER.sparse_pose_graph.constraint_builder.min_score = 0.62
+MAP_BUILDER.sparse_pose_graph.constraint_builder.min_score = 0.45
 
 return options
